eb1b2063a1eb6cd34a52157647c922fcf5770d84
Subbu <svenkat683@gmail.com>
2020-12-30 14:03:11 +0530

    Change the image from background to inline

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
-const typed = new Typed('#typed', {
-  stringsElement: '#typed-strings',
-  typeSpeed: 30,
-  startDelay: 0,
-})
+window.onload = function () {
+  const typed = new Typed('#typed', {
+    stringsElement: '#typed-strings',
+    typeSpeed: 30,
+    startDelay: 0,
+  })
+}
