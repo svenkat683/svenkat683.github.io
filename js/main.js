93b4b68727521571a45bc16bcdd2f87fb3f8fd4f
Subbu <svenkat683@gmail.com>
2020-12-30 20:39:18 +0530

    Adjust the underline

--- js/main.js
+++ js/main.js
@@ -7,9 +7,9 @@ window.onload = function () {
       const mobileMedia = window.matchMedia('(max-width: 560px)')
 
       if (mobileMedia.matches) {
-        document.styleSheets[0].insertRule('@media only screen and (max-width: 35em) { #typed .everest-engineering::after { width: 53% !important; }}', 0);
+        document.styleSheets[0].insertRule('@media only screen and (max-width: 35em) { #typed .everest-engineering::after { width: 54% !important; }}', 0);
       } else {
-        document.styleSheets[0].insertRule('#typed .everest-engineering::after { width: 26% !important; }', 0);
+        document.styleSheets[0].insertRule('#typed .everest-engineering::after { width: 37% !important; }', 0);
       }
     }
   })
