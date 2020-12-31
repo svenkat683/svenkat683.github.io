bdd43720dd6318c7ed17add78e19c4e8a9e3c53e
Subbu <svenkat683@gmail.com>
2020-12-31 23:59:57 +0530

    Add ease in transition

--- js/main.js
+++ js/main.js
@@ -5,12 +5,12 @@ window.onload = function () {
     startDelay: 0,
     onComplete: function () {
       const mobileMedia = window.matchMedia('(max-width: 560px)')
-
       if (mobileMedia.matches) {
         document.styleSheets[0].insertRule('@media only screen and (max-width: 35em) { #typed .everest-engineering::after { width: 54% !important; }}', 0);
       } else {
         document.styleSheets[0].insertRule('#typed .everest-engineering::after { width: 35% !important; }', 0);
       }
+      document.styleSheets[0].insertRule('.social-media-container, .reach-out { opacity: 1 !important; transition: 1s ease-in; }', 0);
     }
   })
 }
